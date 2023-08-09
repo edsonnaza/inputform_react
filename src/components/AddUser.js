1e60bcb466b4c9ab693ec8ad74cea13817c6f714
edsonnaza <edsonnaza@gmail.com>
2023-08-08 23:08:22 -0400

    First Commit

---
+++ src/components/AddUser.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import classes from './AddUser.module.css';
+const AddUser = () =>{
+
+    return (<div>
+                 <h1>Add User Input</h1>
+             </div>);
+}
+
+export default AddUser;
---
+++ src/components/UserList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Userlist = ()=>{
+    return (<div> User List
+
+    </div>);
+}
+
+export default Userlist;
